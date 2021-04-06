879570b9b0a854dfec93c5705c3022f1c41f7558
amimista <amimista.social@gmail.com>
2021-04-05 20:10:31 -0600

    still testing

--- src/Game.java
+++ src/Game.java
@@ -3,9 +3,10 @@ import java.util.ArrayList;
 public class Game {
 
   /*
-  * These are the methods that are performed by the rules of the games.
-  * This involves "hit", "stand", and calclating if the dealer or the player is over 21
-  */
+   * These are the methods that are performed by the rules of the games. This
+   * involves "hit", "stand", and calclating if the dealer or the player is over
+   * 21
+   */
 
   public ArrayList<Integer> setPlayerHand(ArrayList<Integer> targetList) {
     ArrayList<Integer> playerHand = new ArrayList<Integer>();
@@ -27,5 +28,25 @@ public class Game {
     targetList.remove(1);
 
     return dealerHand;
+  }
+
+  public boolean calculate(int int1, int int2) {
+    int resultant = int1 + int2;
+    if (resultant > 21) {
+      System.out.print("You have failed");
+    } else {
+      // TODO main game loop
+    }
+    return true;
+  }
+
+  public boolean calculate(int int1, int int2, int int3) {
+    int resultant = int1 + int2 + int3;
+    if (resultant > 21) {
+      System.out.print("You have failed");
+    } else {
+      // TODO main game loop
+    }
+    return true;
   }
 }
